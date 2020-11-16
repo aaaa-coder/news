30e79b124b41565f39f03a4ea44edc0dfa2a5fed
tangchaofu <1027402361@qq.com>
2020-11-16 15:16:35 +0800

    '项目初始化'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,10 @@
 <template>
-  <div>
-    <h1>这是首页</h1>
-  </div>
+  <div>这是首页</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
+
+<style>
+</style>
