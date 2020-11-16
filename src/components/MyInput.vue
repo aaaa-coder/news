00b603ed917de45e240cdc8f5c107564a5c7fb83
tangchaofu <1027402361@qq.com>
2020-11-16 14:50:28 +0800

    '新增MyInput

---
+++ src/components/MyInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <input type="text" placeholder="请输入内容" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>登录页面</h1>
+    <MyInput />
+    <MyInput />
+  </div>
+</template>
+
+<script>
+import MyInput from "../components/MyInput";
+export default {
+  components: {
+    MyInput,
+  },
+};
+</script>
+
+<style>
+</style>
