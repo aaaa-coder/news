8649977c01665cf9573c7cb59567a91835180c8c
tangchaofu <1027402361@qq.com>
2020-11-16 14:31:24 +0800

    '登录页面路由'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Login from './views/Login.vue'
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  Login,
   render: function (h) { return h(App) }
 }).$mount('#app')
