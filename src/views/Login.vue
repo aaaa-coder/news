c52dd88b0ddfe6c195965ec3b163765c5ffe9fcf
tangchaofu <1027402361@qq.com>
2020-11-17 11:03:52 +0800

    '校验input输入框封装'

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,40 @@
 <template>
   <div class="container">
+    <!-- 关闭按钮 -->
     <div class="closeBtn">
       <i class="iconfont iconicon-test"></i>
     </div>
+    <!-- 登录界面logo -->
     <div class="logoWrapper">
       <i class="iconfont iconnew"></i>
     </div>
-    <div class="inputWrapper">
-      <input type="text" />
-    </div>
-    <div class="inputWrapper">
-      <input type="text" />
-    </div>
+    <!-- 用户名 -->
+    <AuthInput placeholder="请输入用户名/账号" type="text" />
+    <!-- 密码 -->
+    <AuthInput placeholder="请输入密码" type="password" />
+
+    <!-- 登录按钮 -->
     <button class="btn">登录</button>
   </div>
 </template>
 
 <script>
-export default {};
+import AuthInput from "../components/AuthInput";
+export default {
+  components: {
+    AuthInput,
+  },
+};
 </script>
 
 <style lang="less">
 .container {
   padding: 20 /360 * 100vw;
-  // background-color: #f2f2f2;
-
+  // 登录页面关闭按钮
   .iconicon-test {
     font-size: 27 /360 * 100vw;
   }
+  // 登录页面logo
   .logoWrapper {
     text-align: center;
     .iconnew {
@@ -35,25 +42,14 @@ export default {};
       color: #d81e06;
     }
   }
-
-  .inputWrapper {
-    input {
-      width: 100%;
-      border: 0;
-      outline: 0;
-      line-height: 38 /360 * 100vw;
-      margin-top: 10 /360 * 100vw;
-      border-bottom: 1px solid #333;
-      font-size: 18 /360 * 100vw;
-    }
-  }
-
+  // 登录按钮样式
   .btn {
     width: 100%;
     height: 48 /360 * 100vw;
-    margin-top: 30 /360 * 100vw;
+    margin-top: 50 /360 * 100vw;
     border: 0;
     color: #fff;
+    font-size: 20 /360 * 100vw;
     background-color: #cc3300;
     border-radius: 24 /360 * 100vw;
   }
