b23577cf4fd3ff26ddf6ba0585cade5fa8b8f015
tangchaofu <1027402361@qq.com>
2020-11-19 16:30:18 +0800

    '封装content内容部分'

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -21,34 +21,21 @@
     </div>
     <!-- 下面的内容部分 -->
     <div class="content">
-      <div class="userFocus">
-        <span>我的关注</span>
-        <span class="common_focus">关注的用户</span>
-        <i class="iconfont iconjiantou1"></i>
-      </div>
-      <div class="userFocus">
-        <span>我的更贴</span>
-        <span class="common_focus">跟帖/回复</span>
-        <i class="iconfont iconjiantou1"></i>
-      </div>
-      <div class="userFocus">
-        <span>我的收藏</span>
-        <span class="common_focus">视频/文章</span>
-        <i class="iconfont iconjiantou1"></i>
-      </div>
-      <div class="userFocus">
-        <span>设置</span>
-        <i class="iconfont iconjiantou1"></i>
-      </div>
+      <AuthOpeartion title="我的关注" content="关注的内容" />
+      <AuthOpeartion title="我的跟帖" content="跟帖/回复" />
+      <AuthOpeartion title="我的收藏" content="文章/视频" />
+      <AuthOpeartion title="设置" />
     </div>
   </div>
 </template>
 
 <script>
 import CircleImg from "../components/CircleImg";
+import AuthOpeartion from "../components/AuthOpeartion";
 export default {
   components: {
     CircleImg,
+    AuthOpeartion,
   },
 };
 </script>
@@ -87,20 +74,5 @@ export default {
 
 .content {
   padding-left: 10 /360 * 100vw;
-  .userFocus {
-    display: flex;
-    justify-content: space-between;
-    font-size: 16 /360 * 100vw;
-    line-height: 50 /360 * 100vw;
-    border-bottom: 1px solid #ccc;
-    .common_focus {
-      margin-left: 160 /360 * 100vw;
-      font-size: 14 /360 * 100vw;
-      color: #757575;
-    }
-    .iconjiantou1 {
-      margin-right: 10 /360 * 100vw;
-    }
-  }
 }
 </style>
