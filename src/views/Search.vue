f7ed6a5f1b9a08640e397e4948d790793f9707f3
tangchaofu <1027402361@qq.com>
2020-11-23 19:46:08 +0800

    '搜索栏静态搭建完成'

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -8,8 +8,25 @@
       </div>
       <div class="search_right">搜索</div>
     </div>
+    <!-- 历史记录 -->
+    <div class="history">
+      <h4>历史记录</h4>
+      <span class="recode">美女</span>
+    </div>
+
+    <!-- 热门搜索 -->
+    <div class="hot_search">
+      <h4>热门搜索</h4>
+      <span>办公室小野否认解散</span>
+      <span>月季如何嫁接</span>
+      <span>办公室小野否认解散</span>
+      <span>月季如何嫁接</span>
+      <span>办公室小野否认解散</span>
+      <span>月季如何嫁接</span>
+    </div>
   </div>
 </template>
+
 
 <script>
 export default {};
@@ -42,6 +59,28 @@ export default {};
   }
   .search_right {
     margin-left: 7 /360 * 100vw;
+  }
+}
+.history {
+  margin: 20 /360 * 100vw 0;
+  border-bottom: 1px solid #ccc;
+  .recode {
+    display: block;
+    margin: 20 /360 * 100vw 0;
+    font-size: 14 /360 * 100vw;
+  }
+}
+.hot_search {
+  h4 {
+    margin-bottom: 10 /360 * 100vw;
+  }
+  span {
+    display: inline-block;
+    margin: 10 /360 * 100vw 0;
+    font-size: 14 /360 * 100vw;
+    &:nth-child(even) {
+      margin-right: 40 /360 * 100vw;
+    }
   }
 }
 </style>
