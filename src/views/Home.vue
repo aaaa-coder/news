a6b346241d3daaa926f4e07fe9069e2a90120dd5
tangchaofu <1027402361@qq.com>
2020-11-24 14:56:04 +0800

    '首页栏目列表完成'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,47 @@
 <template>
   <div class="HomeContainer">
+    <!-- 首页头部 -->
     <HomeHeader />
+    <!-- 头部新闻列表 -->
+    <van-tabs v-model="activeCategoryIndex">
+      <van-tab
+        :title="category.name"
+        v-for="category in categoryList"
+        :key="category.id"
+      >
+        {{ category.name }}的内容
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 import HomeHeader from "../components/HomeHeader";
 export default {
+  data() {
+    return {
+      activeCategoryIndex: 0,
+      categoryList: [],
+    };
+  },
   components: {
     HomeHeader,
+  },
+  methods: {
+    getCategoryList() {
+      this.$axios({
+        url: "/category",
+      }).then((res) => {
+        if (res.status === 200) {
+          const { data } = res.data;
+          this.categoryList = data;
+          console.log(res);
+        }
+      });
+    },
+  },
+  mounted() {
+    this.getCategoryList();
   },
 };
 </script>
