7faa921cf5dce6b7900091584dca9a2ec943f90b
tangchaofu <1027402361@qq.com>
2020-11-24 15:05:25 +0800

    '添加文章内容组件，利用test进行调试'

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,26 +1,12 @@
 <template>
-  <div class="contariner">
-    <h2>模拟关注</h2>
-    <button @click="follow(1252)">关注用户1</button>
-    <button @click="follow(1061)">关注用户2</button>
-    <button @click="follow(907)">关注用户3</button>
-    <button @click="follow(1)">关注用户4</button>
-  </div>
+  <div class="newsWrapper"></div>
 </template>
 
 <script>
+import PostItem from "../components/PostItem";
 export default {
-  methods: {
-    follow(userId) {
-      //   console.log(userId);
-      this.$axios({
-        url: "/user_follows/" + userId,
-      }).then((res) => {
-        if (res.status === 200) {
-          console.log(res);
-        }
-      });
-    },
+  components: {
+    PostItem,
   },
 };
 </script>
