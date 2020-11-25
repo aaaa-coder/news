cf4d5c237b397f5d26f54b79cb149c28a294fd4d
tangchaofu <1027402361@qq.com>
2020-11-25 17:54:59 +0800

    'test模拟收藏文章与我的收藏功能完成'

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="container">添加文章</div>
+  <div class="container">
+    <h3>模拟收藏</h3>
+    <button @click="collect(10)">收藏文章1</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    collect(id) {
+      this.$axios({
+        url: "/post_star/" + id,
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+  },
+};
 </script>
 
 <style>
