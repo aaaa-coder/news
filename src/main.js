4fceadf7bd11016d4894807b9382d24c37914260
tangchaofu <1027402361@qq.com>
2020-12-05 18:08:03 +0800

    'axios封装完成'

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import Login from './views/Login.vue'
 //引入axios
-import axios from "axios";
+import axios from "./utils/axios";
 //引入vant ui
 import Vant, { Toast } from 'vant';
 import 'vant/lib/index.css';
@@ -11,33 +11,11 @@ Vue.use(Vant);
 //引入uploader
 import { Uploader } from 'vant';
 Vue.use(Uploader);
-//添加axios基地址
-axios.defaults.baseURL = "http://liangwei.tech:3000";
-// axios.defaults.baseURL = "http://157.122.54.189:9083";
-//添加axios请求拦截器
-axios.interceptors.request.use(config => {
-  if (!config.headers.Authorization && localStorage.getItem('token')) {
-    config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
-  }
-  return config;
-})
 
-//添加axios响应拦截器
-axios.interceptors.response.use(res => {
-  //定义返回码的正则
-  const AuthErrReg = /^4\d{2}$/;
-  //进行判断，如果符合就弹出错误信息，进行防御性编程
-  if (AuthErrReg.test(res.data.statusCode)) {
-    Toast.fail(res.data.message || '系统错误');
-  }
 
-  if (res.data.message == '用户信息验证失败') {
-    localStorage.removeItem('token')
-    localStorage.removeItem('userId')
-    router.replace('/login')
-  }
-  return res;
-})
+
+
+
 Vue.filter('setImgUrl', (objUrl) => {
   if (objUrl.indexOf('http') > -1) {
     return objUrl;
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,36 +1,8 @@
- <template>
-  <div>
-    <MainComment
-      :commentData="comment"
-      v-for="(comment, index) in commentList"
-      :key="index"
-    />
-  </div>
-</template>
+<template></template>
 
 <script>
-import MainComment from "../components/Comment/Main";
-export default {
-  components: {
-    MainComment,
-  },
-  data() {
-    return {
-      commentList: [],
-    };
-  },
-  created() {
-    this.$axios({
-      url: "/post_comment/1",
-    }).then((res) => {
-      console.log(res);
-      this.commentList = res.data.data;
-    });
-  },
-};
+export default {};
 </script>
 
 <style>
 </style>
-
- 
